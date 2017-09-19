49d0a8966a2ff5b32642641656c788563ed105fe
Adam Macioszek <amacioszek4@gmail.com>
2017-09-18 20:47:13 -0400

    Rename sketch_sep18a.ino to wall_sensors.ino

---
+++ src/milestone2/wall_sensors.ino
@@ -0,0 +1,43 @@
+#define FRONT_WALL A0
+#define LEFT_WALL A1
+#define RIGHT_WALL A2
+#define THRESHOLD 150
+
+void setup() {
+  pinMode(FRONT_WALL, INPUT);
+  pinMode(LEFT_WALL, INPUT);
+  pinMode(RIGHT_WALL, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115000);
+}
+
+void loop() {
+  int front_dist = analogRead(FRONT_WALL);
+  int left_dist = analogRead(LEFT_WALL);
+  int right_dist = analogRead(RIGHT_WALL);
+  
+  /* Serial.print("Front: ");
+  Serial.println(front_dist);
+  Serial.print("Left: ");
+  Serial.println(left_dist);
+  Serial.print("Right: ");
+  Serial.println(right_dist); */
+
+  if(front_dist > THRESHOLD) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  if(left_dist > THRESHOLD) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  if(right_dist > THRESHOLD) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
