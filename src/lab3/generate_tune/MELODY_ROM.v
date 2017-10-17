66a642f674807ec4fe298b146ce8d960067f9e12
Christina Xu <chx3@cornell.edu>
2017-10-17 17:43:17 -0400

    Add files via upload

---
+++ src/lab3/generate_tune/MELODY_ROM.v
@@ -0,0 +1,40 @@
+//=======================================================
+// ECE3400 Fall 2017
+// Lab 3: Template ROM module
+//
+// Top-level skeleton from Terasic
+// Modified by Claire Chen for ECE3400 Fall 2017
+//=======================================================
+
+module MELODY_ROM(	
+	input [2:0] addr,
+	input clk,
+	output reg [1:0] q
+);
+
+    //=======================================================
+    //  REG/WIRE declarations
+    //=======================================================
+
+	 reg [1:0] melody[6:0];
+	 
+	 initial begin
+		melody[0]  <= 3;
+		melody[1]  <= 2;
+		melody[2]  <= 1;
+		melody[3]  <= 2;
+		melody[4]  <= 3;
+		melody[5]  <= 3;
+		melody[6]  <= 3;
+	 end
+	 
+    //=======================================================
+    //  Structural coding
+    //=======================================================
+ 
+	always @ (posedge clk)
+	begin
+		q <= melody[addr];
+	end
+	 
+endmodule
