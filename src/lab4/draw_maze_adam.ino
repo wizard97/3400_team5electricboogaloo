7c08c9c6d9c790dfc1b6e7099f60361e10cf77b8
Adam Macioszek <amacioszek4@gmail.com>
2017-10-23 21:39:56 -0400

    Add files via upload

---
+++ src/lab4/draw_maze_adam.ino
@@ -0,0 +1,75 @@
+#include <VGAGraphics.h>
+
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+
+VGAGraphics lcd;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.clear();
+  
+  int lwidth = 3;
+  int bwidth = (lcd.width()-5*lwidth)/5;
+  int idx = bwidth/2;
+  int llength = (bwidth+lwidth)*4;
+
+  int y_off = (lcd.height()-llength)/2;
+
+
+  lcd.fillRect(0, y_off, lcd.width(), (lcd.height()-2*y_off), WHITE);
+  for (int i=0; i < 5; i++) {
+    lcd.fillRect(idx, y_off, lwidth, llength, BLACK);
+    idx += lwidth+bwidth;
+  }
+
+  int idy = bwidth/2;
+   for (int i=0; i < 4; i++) {
+    lcd.fillRect(0, y_off+idy, lcd.width(), lwidth, BLACK);
+    idy += lwidth+bwidth;
+  }
+
+  lcd.drawRect(0, y_off, lcd.width(),llength , RED);
+  lcd.fillCircle(y_off, 2*y_off, 10, 0x6969);
+  
+}
+
+void loop() {
+
+    int lwidth = 3;
+  int bwidth = (lcd.width()-5*lwidth)/5;
+  int idx = bwidth/2;
+  int llength = (bwidth+lwidth)*4;
+
+  int y_off = (lcd.height()-llength)/2;
+lcd.fillRect(0, y_off, lcd.width(), (lcd.height()-2*y_off), WHITE);
+  for (int i=0; i < 5; i++) {
+    lcd.fillRect(idx, y_off, lwidth, llength, BLACK);
+    idx += lwidth+bwidth;
+  }
+
+  int idy = bwidth/2;
+   for (int i=0; i < 4; i++) {
+    lcd.fillRect(0, y_off+idy, lcd.width(), lwidth, BLACK);
+    idy += lwidth+bwidth;
+  }
+
+
+  for (int k=2; k < 9; k++){
+    
+     lcd.fillCircle(y_off, k*y_off, 10, 0x6969);
+     delay(2000);
+    }
+
+  
+
+
+}
