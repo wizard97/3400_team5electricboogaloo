b7da43d92b6a51372aad3d52bc626120da38c4fc
kvijar10 <rrk64@cornell.edu>
2017-11-13 22:26:55 -0500

    Add files via upload

---
+++ src/milestone4/enums.h
@@ -0,0 +1,22 @@
+// Enums
+typedef enum State {
+  NO_LINE,
+  ON_LINE,
+  PASSED_LINE,
+} state_t;
+
+typedef enum StateDir {
+  NEGX = 0,
+  NEGY = 1,
+  POSX = 2,
+  POSY = 3
+} state_dir_t;
+
+typedef enum Boundary {
+  LOW_TREASURE,
+  MID_TREASURE,
+  HIGH_TREASURE,
+  WALL,
+  NOTHING,
+  UNKNOWN
+} boundary_t;
